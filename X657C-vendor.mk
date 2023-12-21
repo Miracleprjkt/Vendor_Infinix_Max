ef57357ee8a6df4845a98280109417affd42bce5
Miraclev1 <127039117+Miracleprjkt@users.noreply.github.com>
2023-12-21 21:45:19 +0700

    Update X657C-vendor.mk

--- X657C-vendor.mk
+++ X657C-vendor.mk
@@ -68,3 +68,6 @@ PRODUCT_PACKAGES += \
     mediatek-telecom-common \
     mediatek-telephony-base \
     mediatek-telephony-common
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
